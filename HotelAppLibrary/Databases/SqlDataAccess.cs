48315d12335d477f8ebad0863db244d9638db8f2
Thiago da silva <acolweb16@outlook.com>
2023-09-27 11:53:03 -0300

    Criando uma class library para acesso a dados, instalando dapper e criando classe pra acessar

---
+++ HotelAppLibrary/Databases/SqlDataAccess.cs
@@ -0,0 +1,46 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+
+namespace HotelAppLibrary.Databases
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _config;
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public List<T> LoadData<T, U>(string sql, U parameters, string connectionStringName, bool isStoredProcedure = false)
+        {
+            string? connectionString = _config.GetConnectionString(connectionStringName);
+            CommandType commandType = CommandType.Text;
+
+            if (isStoredProcedure)
+            {
+                commandType = CommandType.StoredProcedure;
+            }
+
+            using IDbConnection connection = new SqlConnection(connectionString);
+            var rows = connection.Query<T>(sql, parameters, commandType: commandType).ToList();
+            return rows;
+        }
+
+        public void SaveData<T>(string sql, T parameters, string connectionStringName, bool isStoredProcedure = false)
+        {
+            string? connectionString = _config.GetConnectionString(connectionStringName);
+            CommandType commandType = CommandType.Text;
+
+            if (isStoredProcedure)
+            {
+                commandType = CommandType.StoredProcedure;
+            }
+
+            using IDbConnection connection = new SqlConnection(connectionString);
+            connection.Execute(sql, parameters, commandType: commandType);
+        }
+    }
+}
